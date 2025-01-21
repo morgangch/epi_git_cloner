e3ccbcbb08dafe950945f8b36730719af5118d9d
Morgan <morgan.guichard@epitech.eu>
2025-01-21 12:04:30 +0100

    ADD Pool Days handling

--- git_cloner.sh
+++ git_cloner.sh
@@ -65,7 +65,7 @@ function project {
     fi
 
     # Demander à l'utilisateur de saisir le numéro du module
-    module_list=("PSU" "CPE" "AIA" "MAT" "MUL" "DOP" "SEC" "PRO" "WEB")
+    module_list=("PSU" "CPE" "AIA" "MAT" "MUL" "DOP" "SEC" "PRO" "WEB" "PDG")
     module=$(prompt_user "Entrez le numéro du module (entre 1 et 20)
             1 - PSU
             2 - CPE
@@ -76,6 +76,7 @@ function project {
             7 - SEC
             8 - PRO
             9 - WEB
+	    10 - PDG
             ")
     while ! is_valid_input "$module" 1 20; do
         echo "Erreur: Veuillez entrer un numéro de module valide (entre 1 et 20)."
@@ -210,16 +211,86 @@ function perso {
     esac
 }
 
+function is_number_greater_than_ten {
+    local input=$1
+
+    # Supprimer tous les caractères qui ne sont pas des chiffres
+    local num=$(echo "$input" | tr -cd '0-9')
+
+    # Si la chaîne est vide après nettoyage, définir à 0
+    if [ -z "$num" ]; then
+        num=0
+    fi
+
+    # Comparer si le nombre est supérieur à 10
+    if [ "$num" -gt 10 ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function big_piscine
+{
+    local module_name=$1
+    local repo_num=$2
+    local semester=$3
+    local project_name=$4
+
+    semester2=$semester
+    if ((semester < 10)); then
+        semester2="0$semester2"
+    fi
+    leader_fname=$(echo "$fname" | tr '[:upper:]' '[:lower:]')
+    leader_name=$(echo "$name" | tr '[:upper:]' '[:lower:]')
+    leader="$leader_fname.$leader_name"
+
+    get_pwd $semester2 $module_name
+
+    pool_day=$(prompt_user "Day de Piscine")
+    if (is_number_greater_than_ten "$pool_day"); then
+        pool_day="0$pool_day"
+    fi
+    project_name="$project_name$pool_day"
+    cd $full_pwd
+    $gitswitch_path $user1
+    repo="B-$module_name-$repo_num-$city-$semester-1-$project_name-$leader"
+    git clone "git@github.com:EpitechPromo$promo/$repo"
+    mv $repo $project_name
+    cd $project_name
+}
+
+function piscine {
+    echo "Quelle piscine faites-vous ?"
+    echo "1 - Tek0"
+    echo "2 - Tek2"
+    choice=$(prompt_user "Entrez votre choix (1 ou 2)")
+    while ! is_valid_input "$choice" 1 2; do
+        echo "Erreur: Veuillez entrer un choix valide (1 ou 2)."
+        choice=$(prompt_user "Entrez votre choix (1 ou 2)")
+    done
+
+    case $choice in
+        1)
+            big_piscine "CPE" 100 1 "CPOOLDAY"
+            ;;
+        2)
+            big_piscine "PDG" 300 3 "PDGD"
+            ;;
+    esac
+}
+
 function main {
     echo "Que voulez-vous cloner ?"
     echo "1 - Projet"
     echo "2 - Stumper"
     echo "3 - Projet perso"
-    echo "4 - Quitter"
-    choice=$(prompt_user "Entrez votre choix (1, 2, 3 ou 4)")
-    while ! is_valid_input "$choice" 1 4; do
-        echo "Erreur: Veuillez entrer un choix valide (1, 2, 3 ou 4)."
-        choice=$(prompt_user "Entrez votre choix (1, 2, 3 ou 4)")
+    echo "4 - Piscine"
+    echo "5 - Quitter"
+    choice=$(prompt_user "Entrez votre choix (1, 2, 3, 4 ou 5)")
+    while ! is_valid_input "$choice" 1 5; do
+        echo "Erreur: Veuillez entrer un choix valide (1, 2, 3, 4 ou 5)."
+        choice=$(prompt_user "Entrez votre choix (1, 2, 3, 4 ou 5)")
     done
 
     case $choice in
@@ -233,6 +304,9 @@ function main {
             perso
             ;;
         4)
+            piscine
+            ;;
+        5)
             return
             ;;
     esac
